6313142d6e1a9b27b29dfc342c25bb6d792bf625
MUHAMMET FURKAN ALTUN <muhammetfurkan.altun@stu.khas.edu.tr>
2021-01-31 16:18:52 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine( $"Marka: {item.BrandId} | Renk: {item.ColorId} | Fiyat: {item.DailyPrice} | Yıl: {item.ModelYear} | Açıklama: {item.Description}");
+            }
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryBrandDal.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryBrandDal : IBrandDal
+    {
+        List<Brand> _brands;
+
+        public InMemoryBrandDal()
+        {
+            _brands = new List<Brand>
+            {
+                new Brand{BrandId = 1, BrandName ="Mercedes"},
+                new Brand{BrandId = 2, BrandName ="Bmw"},
+                new Brand{BrandId = 3, BrandName ="Ferrari"}
+            };
+        }
+
+        public List<Brand> GetAll()
+        {
+            return _brands;
+        }
+    }
+}
